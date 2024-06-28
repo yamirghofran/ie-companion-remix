5cdc62d26203c156ef08c2680e937e75f3df504e
Yousef Amirghofran <yamirghofran@gmail.com>
2024-06-28 11:26:23 +0200

    Update schema

--- scripts/import-csv.ts
+++ scripts/import-csv.ts
@@ -3,6 +3,7 @@ import fs from 'fs';
 import { parse } from 'csv-parse';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import { CreateResourceInput, CreateResourceItemInput } from '~/types/Resource';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -48,7 +49,61 @@ async function importReviews() {
     });
 }
 
-importReviews().catch((e) => {
+/* importReviews().catch((e) => {
   console.error(e);
   process.exit(1);
+});
+ */
+
+async function importResource(resourceData: CreateResourceInput) {
+  try {
+    const resource = await prisma.resource.create({
+      data: {
+        name: resourceData.name,
+        description: resourceData.description,
+        content: resourceData.content,
+        type: resourceData.type,
+      },
+    });
+    console.log(`Resource created successfully with id: ${resource.id}`);
+    return resource;
+  } catch (error) {
+    console.error('Error creating resource:', error);
+    throw error;
+  }
+}
+
+async function importResourceItem(itemData: CreateResourceItemInput) {
+  try {
+    const resourceItem = await prisma.resourceItem.create({
+      data: {
+        name: itemData.name,
+        description: itemData.description,
+        url: itemData.url,
+        type: itemData.type,
+        resourceId: itemData.resourceId,
+      },
+    });
+    console.log(`ResourceItem created successfully with id: ${resourceItem.id}`);
+    return resourceItem;
+  } catch (error) {
+    console.error('Error creating resource item:', error);
+    throw error;
+  }
+}
+
+
+const newResource = await importResource({
+  name: 'Academic Calender 2024-2025',
+  description: 'Academic Calender for the year 2024-2025',
+  content: '',
+  type: 'document',
+});
+
+const newResourceItem = await importResourceItem({
+  name: 'Academic Calender 2024-2025',
+  description: 'Academic Calender for the year 2024-2025',
+  url: 'https://d3mm5s1n33q4os.cloudfront.net/IEU-Academic-Calendar-2024-2025.pdf',
+  type: 'pdf',
+  resourceId: newResource.id,
 });
